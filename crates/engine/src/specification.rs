d7b4718c5d313ffab9614f7e1ec17edf835c0627
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-12 22:17:25 +0200

    feat(engine): working on specification

--- crates/engine/src/specification.rs
+++ crates/engine/src/specification.rs
@@ -1,14 +1,81 @@
 use crate::net::Net;
 use crate::version::Version;
+use quick_xml::reader::Reader;
+use serde::{Deserialize, Serialize};
+use std::io;
 
 /// # Specification
 /// the outer container of a process model. Such an object is a process
 /// specification/model. A process specification in YAWL basically contains a set of Nets (process nets)
-struct Specification {
-    id: String,
-    version: Version,
-    description: String,
-    root_net: Net,
+/// TODO: write custom serde serializer and deserializer instead of using attributes
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Specification {
+    /// unique identifier of a specification
+    #[serde(rename = "@id")]
+    pub id: String,
+
+    /// specification version
+    /// format: `major.minor`
+    #[serde(rename = "@version")]
+    pub version: Version,
+
+    /// specification name
+    #[serde(rename = "@name")]
+    pub name: Option<String>,
+
+    /// short text that describe, what does the specification do
+    /// and what is the use case of the specification
+    #[serde(rename = "@description")]
+    pub description: Option<String>,
+
+    pub root_net: Net,
 }
 
-impl Specification {}
+impl Specification {
+    pub fn new(id: String, version: Version, root_net: Net) -> Self {
+        Self { id, version, name: None, description: None, root_net }
+    }
+
+    pub fn parse<R>(reader: R) -> Result<Specification, io::Error>
+    where
+        R: io::Read,
+    {
+        let mut xml_reader = Reader::from_reader(reader);
+        xml_reader.config_mut().trim_text(true);
+
+        let count = 0;
+        let buf = Vec::new();
+        todo!()
+    }
+}
+
+
+#[test]
+fn test_specification_parsing() {
+    let spec = r#"
+    <Specification
+        id="test-specification-id"
+        name="test-specification-name"
+        version="0.1"
+        description="test-specification-description">
+        <RootNet>
+            <InputCondition id="input-condition">
+                <FlowsInto ref="greet" default="true"/>
+                <FlowsInto ref="greet-by-name">
+                    <Predicate>false</Predicate>
+                </FlowsInto>
+            </InputCondition>
+            <Task id="greet">
+                <FlowsInto ref="output-condition"/>
+            </Task>
+            <Task id="greet-by-name">
+                <FlowsInto ref="output-condition"/>
+            </Task>
+            <OutputCondition id="output-condition"/>
+        </RootNet>
+    </Specification>
+    "#;
+}
+
+
--- crates/engine/src/version.rs
+++ crates/engine/src/version.rs
@@ -1,8 +1,10 @@
 /// # Version
+/// TODO: make versions comparable
+/// TODO: is it good to make field public ?
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Version {
-    major: u64,
-    minor: u64,
+    pub major: u64,
+    pub minor: u64,
 }
 
 impl Version {
@@ -10,8 +12,16 @@ impl Version {
         let digits: Vec<&str> = version.split('.').collect();
         match digits.len() {
             2 => {
-                let major = digits[0].parse::<u64>().unwrap();
-                let minor = digits[1].parse::<u64>().unwrap();
+                let major = match digits[0].parse::<u64>() {
+                    Ok(n) => n,
+                    Err(_) => return Err("failed to parse major version"),
+                };
+
+                let minor = match digits[1].parse::<u64>() {
+                    Ok(n) => n,
+                    Err(_) => return Err("failed to parse minor version"),
+                };
+
                 Ok(Version { major, minor })
             }
             _ => {
@@ -31,8 +41,30 @@ fn test_parse_valid_version() {
 
 
 #[test]
-fn test_parse_invalid_version() {
+fn test_parse_version_with_too_many_dots() {
     let string_version = "1.1.1".to_string();
     let version = Version::parse(string_version);
     assert!(version.is_err());
+}
+
+#[test]
+fn test_parse_version_with_no_digits() {
+    let string_version = ".".to_string();
+    let version = Version::parse(string_version);
+    assert!(version.is_err());
+}
+
+#[test]
+fn test_parse_version_with_no_dots() {
+    let string_version = "1".to_string();
+    let version = Version::parse(string_version);
+    assert!(version.is_err());
+}
+
+
+#[test]
+fn test_parse_version_with_multiple_digits() {
+    let string_version = "11.11".to_string();
+    let version = Version::parse(string_version);
+    assert_eq!(version, Ok(Version { major: 11, minor: 11 }));
 }
