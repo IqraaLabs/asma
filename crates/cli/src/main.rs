97ec0bd6067068da30cfbac3112afc31cdfdcab2
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-12 22:20:58 +0200

    update

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,12 +1,32 @@
-use clap::Command;
-
 fn main() {
-    let cli = Command::new("asma")
-        .bin_name("asma")
-        .about("asma workflow engine command line interface")
-        .subcommand_required(true)
-        .subcommand(Command::new("workflow").about("run workflow").alias("wf"))
-        .subcommand(Command::new("list").about("list running").alias("ls"));
+    // let cli = Command::new("asma")
+    //     .bin_name("asma")
+    //     .about("asma workflow engine command line interface")
+    //     .subcommand_required(true)
+    //     .subcommand(Command::new("workflow").about("run workflow").alias("wf"))
+    //     .subcommand(Command::new("list").about("list running").alias("ls"));
+    //
+    for item in 0..=10 {
+        println!("item: {}", item);
+    }
 
-    let matches = cli.get_matches();
+    let result = add_two_numbers(2, 2);
+    println!("res {}", result);
+
+    let x = if true { true } else { false };
+
+    let y = match true {
+        true => true,
+        false => false,
+    };
+}
+
+
+/// add two integers
+///
+/// # params
+/// - `x` first integer
+/// - `y` second integer
+fn add_two_numbers(x: u32, y: u32) -> u32 {
+    x + y
 }
